7d9404e2f2d0e172f838392c4a72fa7dcb864b48
galjabari <72796602+galjabari@users.noreply.github.com>
2024-06-04 11:45:58 -0700

    Update install-gns3.sh

--- install-gns3.sh
+++ install-gns3.sh
@@ -10,7 +10,7 @@ sudo usermod -aG libvirt $USER
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 sudo apt-get install curl -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" -y
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt update
 sudo apt install docker-ce -y
 sudo usermod -aG docker $USER 
