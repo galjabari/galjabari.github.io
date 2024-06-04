3798bf1795e70b9f7509c7a1af5d52dca314de0f
Ghannam Aljabari <galjabari@gmail.com>
2024-06-04 15:55:15 +0300

    .

---
+++ install-gns3.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+## Install GNS3
+sudo add-apt-repository ppa:gns3/ppa -y
+sudo apt update
+sudo apt install gns3-gui gns3-server -y
+sudo apt install tigervnc-viewer -y 
+sudo usermod -aG ubridge $USER 
+sudo usermod -aG libvirt $USER
+## Install Docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install curl -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" -y
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker $USER 
