015f3d3951e92cead65015094cb3ea34dd881829
galjabari <72796602+galjabari@users.noreply.github.com>
2024-06-04 12:35:57 -0700

    Update install-gns3.sh

--- install-gns3.sh
+++ install-gns3.sh
@@ -3,9 +3,9 @@
 sudo add-apt-repository ppa:gns3/ppa -y
 sudo apt update
 sudo apt install gns3-gui gns3-server -y
-sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt install gns3-iou -y
+#sudo dpkg --add-architecture i386
+#sudo apt update
+#sudo apt install gns3-iou -y
 sudo apt install tigervnc-viewer -y 
 sudo usermod -aG ubridge $USER 
 sudo usermod -aG libvirt $USER
