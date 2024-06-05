489439c0e38bf12c74286305d30e38514159c9b6
galjabari <72796602+galjabari@users.noreply.github.com>
2024-06-05 12:18:08 -0700

    Update install-gns3.sh

--- install-gns3.sh
+++ install-gns3.sh
@@ -14,4 +14,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt update
 sudo apt install docker-ce -y
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $(whoami)
