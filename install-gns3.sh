34a31daf4cbd2e3e533df30c97ab8acedf5a89d4
Ghannam Aljabari <galjabari@gmail.com>
2025-07-21 11:44:23 +0300

    .

--- install-gns3.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-sudo add-apt-repository ppa:gns3/ppa -y
-sudo apt update
-sudo apt install gns3-gui gns3-server -y
-#sudo dpkg --add-architecture i386
-#sudo apt update
-#sudo apt install gns3-iou -y
-sudo apt install tigervnc-standalone-server tigervnc-viewer -y
